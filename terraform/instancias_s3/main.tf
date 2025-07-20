672bb093ba40bd206fd503d513084b96ccb60a8d
marcospaulopitta <marcospaulopitta@gmail.com>
2025-07-20 16:09:31 -0300

    ajustes nas referencias do terraform

--- terraform/instancias_s3/main.tf
+++ terraform/instancias_s3/main.tf
@@ -4,18 +4,18 @@ provider "aws" {
 variable "bucket_name" {
   type = string
 }
-resource "aws_s3_bucket" "redirecionador" {
-  bucket = "bucket-redirecionador-${var.bucket_name}"
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "bucket-s3-${var.bucket_name}"
 
   tags = {
-    Name        = "Redirecionador S3"
+    Name        = "Bucket_S3"
     Environment = "Desenvolvimento"
     Owner       = "Marcos Pitta"
   }
 }
 
-resource "aws_s3_bucket_website_configuration" "redirecionador_website" {
-  bucket = aws_s3_bucket.redirecionador.id
+resource "aws_s3_bucket_website_configuration" "s3_bucket_website_configuration" {
+  bucket = aws_s3_bucket.s3_bucket.id
 
   index_document {
     suffix = "index.html"
@@ -25,8 +25,8 @@ resource "aws_s3_bucket_website_configuration" "redirecionador_website" {
     key = "404.html"
   }
 }
-resource "aws_s3_bucket_public_access_block" "redirecionador_public_access_block" {
-  bucket = aws_s3_bucket.redirecionador.id
+resource "aws_s3_bucket_public_access_block" "bucket_public_access_block" {
+  bucket = aws_s3_bucket.s3_bucket.id
 
   block_public_acls       = false
   block_public_policy     = false
@@ -34,18 +34,18 @@ resource "aws_s3_bucket_public_access_block" "redirecionador_public_access_block
   restrict_public_buckets = false
   
 }
-resource "aws_s3_bucket_ownership_controls" "redirecionador" {
-    bucket = aws_s3_bucket.redirecionador.id
+resource "aws_s3_bucket_ownership_controls" "bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
     
     rule {
         object_ownership = "BucketOwnerPreferred"
     }
 }
-resource "aws_s3_bucket_acl" "redirecionador_acl" {
-  bucket = aws_s3_bucket.redirecionador.id
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.s3_bucket.id
   depends_on = [ 
-    aws_s3_bucket_public_access_block.redirecionador_public_access_block,
-    aws_s3_bucket_ownership_controls.redirecionador,
+    aws_s3_bucket_ownership_controls.bucket,
+    aws_s3_bucket_public_access_block.bucket_public_access_block
    ]
   acl = "public-read"
 }
